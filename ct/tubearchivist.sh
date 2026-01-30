c923f8f0c771919e2d618512cccaf333e6899336
Sebastian Klocke <sebastian.kuhnow@pm.me>
2026-01-30 22:13:07 +0100

    init tubearchivist

---
+++ ct/tubearchivist.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2026 community-scripts ORG
+# Author: skuhnow
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://tududi.com
+
+APP="Tubearchivist"
+var_tags="${var_tags:-media;youtube}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-4096}"
+var_disk="${var_disk:-40}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-13}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
